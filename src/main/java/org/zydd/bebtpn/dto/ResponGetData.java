8d913503d06adf09a8bf089b4505e0498c4b6641
callmezydd <zudaboys@gmail.com>
2024-07-15 15:51:29 +0700

    - adjust ResponGetData type data for atribut data

--- src/main/java/org/zydd/bebtpn/dto/ResponGetData.java
+++ src/main/java/org/zydd/bebtpn/dto/ResponGetData.java
@@ -4,13 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.zydd.bebtpn.entity.Customers;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponGetData {
+public class ResponGetData<T> {
     private ResponHeader header;
-    private Customers data;
+    private T data;
 }
